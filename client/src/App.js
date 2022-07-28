100c0803bd15407b18e59f74118089fc3b11480a
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-07-27 22:54:08 -0500

    app/index-general-style

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Landing from "./components/landing/Landing.jsx";
+import Home from "./components/home/Home.jsx";
+import PokemonDetail from "./components/pokemonDetail/PokemonDetail.jsx";
+import Create from "./components/create/Create.jsx";
+// import About from "./components/about/About.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
+        <Route path="/pokemon/:id" component={PokemonDetail} />
+        <Route path="/create" component={Create} />
+        {/* <Route path="/about" component={About} /> */}
+      </Switch>
+    </React.Fragment>
   );
 }
 
