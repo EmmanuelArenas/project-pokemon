c50098edacbfe0a1ec0ec92ea2166f6c241fd252
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-07-27 20:37:37 -0500

    models-and-relation

--- api/src/db.js
+++ api/src/db.js
@@ -42,12 +42,8 @@ const { Pokemon, Type } = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
-Pokemon.belongsToMany(Type, {
-  through: "pokemon_type",
-});
-Type.belongsToMany(Pokemon, {
-  through: "pokemon_type",
-});
+Pokemon.belongsToMany(Type, { through: "pokemon_type" });
+Type.belongsToMany(Pokemon, { through: "pokemon_type" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
