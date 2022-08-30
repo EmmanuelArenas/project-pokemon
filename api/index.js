baec540c99eb78549e7d12e5d39930259f06ca1b
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-08-30 17:11:10 -0500

    configurations deploy update index.js

--- api/index.js
+++ api/index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
